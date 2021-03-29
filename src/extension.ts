3f73c57eba75c314947d3a9276560436073a1f8d
Mohamed-Fathy-Salah <mofasa.peace@gmail.com>
2021-03-29 22:38:54 +0200

    resolvecompletion item is not working

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,36 @@
 // Import the module and reference it with the alias vscode in your code below
 
 
-import vscode from 'vscode';
+import vscode, { CompletionItem } from 'vscode';
 import arr from "./bindings.json";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 //https://github.com/microsoft/vscode-extension-samples/blob/main/completions-sample/src/extension.ts
+let x = -1;
 export function activate(context: vscode.ExtensionContext) {
+
 	const provider1 = vscode.languages.registerCompletionItemProvider('cpp', {
-		provideCompletionItems(document, position, token) {
-				//todo :str = conv(token);
-				let end = position.character;
-				let start = end-1;
-				let line = document.lineAt(position.line).text; 
-				while(start>0 && line[start]>='a'&&line[start]<='z')start--;
-				if(line[start] <'a'||line[start]>'z')start++;
-				let str = line.substring(start,end);
-				// console.clear();
-				// console.log(conv(str));
-				return [new vscode.CompletionItem(conv(str))];
+		provideCompletionItems(document, position) {
+			//todo :str = conv(token);
+			let end = position.character;
+			let start = end - 1;
+			let line = document.lineAt(position.line).text;
+			while (start > 0 && line[start] >= 'a' && line[start] <= 'z') start--;
+			if (line[start] < 'a' || line[start] > 'z') start++;
+			let str = line.substring(start, end);
+			// console.clear();
+			console.log("---" + conv(str));
+			const comp = new vscode.CompletionItem(conv(str));
+			return [comp];
+		}, resolveCompletionItem(item, token) {
+			console.log("---there");
+			return item;
 		}
 	});
 	context.subscriptions.push(provider1);
+	// context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(()=>{
+	// 	console.log("---shit");
+	// }));
 }
 
 // this method is called when your extension is deactivated
@@ -33,30 +42,30 @@ this method convert symbols to its mapping
 sample input : "mviplld"
 sample output: "map<vector<int>,pair<long long,double>>"
 */
-function conv (str:string):string{
-	let ret =[""];
+function conv(str: string): string {
+	let ret = [""];
 	let stack = [];
 	let i = 0;
-	while(i<str.length){
-		while(i<str.length && !(ret[ret.length-1] in arr))ret[ret.length-1]+=str[i++];
-		let cur = ret[ret.length-1] as keyof typeof arr;
-		if(!(cur in arr))return "";
-		let n= arr[cur].number;
-		ret[ret.length-1] =arr[cur].value;
-		if(n){
+	while (i < str.length) {
+		while (i < str.length && !(ret[ret.length - 1] in arr)) ret[ret.length - 1] += str[i++];
+		let cur = ret[ret.length - 1] as keyof typeof arr;
+		if (!(cur in arr)) return "";
+		let n = arr[cur].number;
+		ret[ret.length - 1] = arr[cur].value;
+		if (n) {
 			ret.push('<');
 			stack.push(n);
-		}else if(stack.length){
-			while(stack.length && stack[stack.length-1]==1){
+		} else if (stack.length) {
+			while (stack.length && stack[stack.length - 1] == 1) {
 				stack.pop();
 				ret.push('>');
 			}
-			if(stack.length && stack[stack.length-1] ==2){
-				stack[stack.length-1]--;
+			if (stack.length && stack[stack.length - 1] == 2) {
+				stack[stack.length - 1]--;
 				ret.push(',');
 			}
 		}
 		ret.push("");
 	}
-	return ret.join("")+'>'.repeat(stack.length);
+	return ret.join("") + '>'.repeat(stack.length);
 }
